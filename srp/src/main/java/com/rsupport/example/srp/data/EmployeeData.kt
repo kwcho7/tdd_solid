6206ff6c5ec469f7bc7794483ad64fc067f1450c
chokyuwon <kwcho@mycreditchain.io>
2020-06-01 06:51:51 +0900

    srp data

---
+++ srp/src/main/java/com/rsupport/example/srp/data/EmployeeData.kt
@@ -0,0 +1,3 @@
+package com.rsupport.example.srp.data
+
+data class EmployeeData(val pay: Int = 0, val hours: Int = 0)
---
+++ srp/src/test/java/com/rsupport/example/srp/data/EmployeeDataTest.kt
@@ -0,0 +1,22 @@
+package com.rsupport.example.srp.data
+
+import org.hamcrest.core.Is.`is`
+import org.junit.Assert.assertThat
+import org.junit.Test
+
+class EmployeeDataTest{
+
+    @Test
+    fun getPay() {
+        val employeeData = EmployeeData(1000)
+        val pay = employeeData.pay
+        assertThat("입력된 pay 가 달라서 실패", pay, `is`(1000))
+    }
+
+    @Test
+    fun getHours() {
+        val employeeData = EmployeeData(hours = 8)
+        val hours = employeeData.hours
+        assertThat("입력된 hours 가 달라서 실패", hours, `is`(8))
+    }
+}
