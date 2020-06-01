63eaeb382e7ef2b23d39453ea8871c2f8702d8f1
chokyuwon <kwcho@mycreditchain.io>
2020-06-02 06:28:26 +0900

    srp EmployeeFacade.kt > payCalculator

---
+++ srp/src/main/java/com/rsupport/example/srp/facade/EmployeeFacade.kt
@@ -0,0 +1,13 @@
+package com.rsupport.example.srp.facade
+
+import com.rsupport.example.srp.database.EmployeeDatabase
+
+class EmployeeFacade(private val id: Int, private val employeeDatabase: EmployeeDatabase) {
+
+    fun calculatorPay(): Int {
+        return employeeDatabase.find(id)?.let {
+            PayCalculator(it).calculatePay()
+        } ?: 0
+    }
+
+}
---
+++ srp/src/test/java/com/rsupport/example/srp/facade/EmployeeFacadeTest.kt
@@ -0,0 +1,32 @@
+package com.rsupport.example.srp.facade
+
+import com.rsupport.example.srp.data.EmployeeData
+import com.rsupport.example.srp.database.EmployeeDatabase
+import org.hamcrest.Matchers
+import org.junit.After
+import org.junit.Before
+
+import org.junit.Assert.*
+import org.junit.Test
+
+class EmployeeFacadeTest {
+    private val employeeID = 0
+    private val employeeDatabase = EmployeeDatabase()
+
+    @Before
+    fun setUp() {
+        employeeDatabase.update(EmployeeData(employeeID, 1000, 8))
+    }
+
+    @After
+    fun tearDown() {
+    }
+
+
+    @Test
+    fun calculatorPay() {
+        val employeeFacade = EmployeeFacade(employeeID, employeeDatabase)
+        val pay = employeeFacade.calculatorPay()
+        assertThat("pay 값이 달라서 실패", pay, Matchers.`is`(8000))
+    }
+}
