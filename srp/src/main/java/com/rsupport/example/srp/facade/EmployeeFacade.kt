f2da45725773e34c16f007e0e077eb6dbd306253
chokyuwon <kwcho@mycreditchain.io>
2020-06-02 06:30:01 +0900

    srp EmployeeFacade.kt > reportHours

--- srp/src/main/java/com/rsupport/example/srp/facade/EmployeeFacade.kt
+++ srp/src/main/java/com/rsupport/example/srp/facade/EmployeeFacade.kt
@@ -10,4 +10,11 @@ class EmployeeFacade(private val id: Int, private val employeeDatabase: Employee
         } ?: 0
     }
 
+    fun reportHours(): String {
+        return employeeDatabase.find(id)?.let {
+            HourReporter(it).reportHours()
+        } ?: ""
+    }
+
+
 }
--- srp/src/test/java/com/rsupport/example/srp/facade/EmployeeFacadeTest.kt
+++ srp/src/test/java/com/rsupport/example/srp/facade/EmployeeFacadeTest.kt
@@ -1,7 +1,9 @@
 package com.rsupport.example.srp.facade
 
+import com.rsupport.example.srp.bad.Employee
 import com.rsupport.example.srp.data.EmployeeData
 import com.rsupport.example.srp.database.EmployeeDatabase
+import org.hamcrest.CoreMatchers
 import org.hamcrest.Matchers
 import org.junit.After
 import org.junit.Before
@@ -28,5 +30,12 @@ class EmployeeFacadeTest {
         val employeeFacade = EmployeeFacade(employeeID, employeeDatabase)
         val pay = employeeFacade.calculatorPay()
         assertThat("pay 값이 달라서 실패", pay, Matchers.`is`(8000))
+    }
+
+    @Test
+    fun reportHours() {
+        val employee = EmployeeFacade(employeeID, employeeDatabase)
+        val report = employee.reportHours()
+        assertThat("hour 값이 달라서 실패", report, CoreMatchers.containsString("8"))
     }
 }
